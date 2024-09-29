0bde1cdb348a55d5137094ef6eb0939e19eda631
hjoo830 <hjoo830@naver.com>
2024-09-29 19:44:10 +0900

    ✨ Feat: Disable + and - buttons when on/off button is clicked

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,12 +12,14 @@ function App() {
         <div>
           <button
             data-testid="minus-button"
+            disabled={disabled}
             onClick={() => setCount((count) => count - 1)}
           >
             -
           </button>
           <button
             data-testid="plus-button"
+            disabled={disabled}
             onClick={() => setCount((count) => count + 1)}
           >
             +
@@ -25,6 +29,7 @@ function App() {
           <button
             data-testid="on/off-button"
             style={{ backgroundColor: "blue" }}
+            onClick={() => setDisabled((disabled) => !disabled)}
           >
             on/off
           </button>
